486daf182d3a97aa8af8268b0457e817097f74ff
shubhamCybage16 <Mycybage1@>
2021-06-16 11:54:51 +0530

    api created to fetch data from database

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import HamburgerDrawer from "./HamburgerDrawer";
 
 function App() {
+  const [category, setCategory] = useState("General");
+  const [newsCity, setNewsCity] = useState("Meerut");
+  const [allCategory, setAllCategory] = useState([]);
+  const [allNewsCity, setAllNewsCategory] = useState([]);
+  console.log(process.env);
+  var myvar;
+  // Here we will write code to call backend api using axios
+  const newsApi = async () => {
+    try {
+      const myAllCategories = await axios.get(
+        `http://localhost:3001/newsBlogs/allCategory`
+      );
+      myvar = myAllCategories;
+      console.log(myAllCategories);
+      myAllCategories.data.map((cat) => {
+        setAllCategory((arr) => [...arr, cat.category]);
+      });
+      console.log(allCategory);
+      const myAllNewsCity = await axios.get(
+        `http://localhost:3001/newsBlogs/allNewsCity`
+      );
+      myAllNewsCity.data.map((cat) => {
+        setAllNewsCategory((arr) => [...arr, cat.newsCity]);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(myvar);
+  useEffect(() => {
+    newsApi();
+    // eslint-disable-next-line
+  }, [category, newsCity]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="#home">
+      <HamburgerDrawer
+        setCategory={setCategory}
+        setNewsCity={setNewsCity}
+        allCategory={allCategory}
+        allNewsCity={allNewsCity}
+      />
     </div>
   );
 }
