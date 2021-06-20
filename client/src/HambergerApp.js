79f5fe242bfc58df2281ed0872e2c4b3aa032300
shubhamCybage16 <Mycybage1@>
2021-06-20 21:14:21 +0530

    side bar fully completed

--- client/src/HambergerApp.js
+++ client/src/HambergerApp.js
@@ -4,8 +4,6 @@ import "./HambergerApp.css";
 import HamburgerDrawer from "./HamburgerDrawer";
 
 function HambergerApp() {
-  //const [category, setCategory] = useState("General");
-  //const [newsCity, setNewsCity] = useState("Meerut");
   const [catAndCity, setCatAndCity] = useState({
     category: "",
     city: "",
@@ -14,23 +12,18 @@ function HambergerApp() {
   const [allNewsCity, setAllNewsCategory] = useState([]);
   console.log(process.env);
 
-  // Here we will write code to call backend api using axios
+  // Here is the code call backend api using axios
   const newsApi = async () => {
     try {
       const myAllCategories = await axios.get(
         `http://localhost:3001/newsBlogs/allCategory`
       );
-
-      console.log(myAllCategories);
-      // eslint-disable-next-line array-callback-return
       myAllCategories.data.map((cat) => {
         setAllCategory((arr) => [...arr, cat.category]);
       });
-      console.log(allCategory);
       const myAllNewsCity = await axios.get(
         `http://localhost:3001/newsBlogs/allNewsCity`
       );
-      // eslint-disable-next-line array-callback-return
       myAllNewsCity.data.map((cat) => {
         setAllNewsCategory((arr) => [...arr, cat.newsCity]);
       });
@@ -38,17 +31,12 @@ function HambergerApp() {
       console.log(error);
     }
   };
-  console.log(catAndCity);
   useEffect(() => {
     newsApi();
-    // eslint-disable-next-line
   }, []);
-  //id="#home"
   return (
     <div className="HambergerApp">
       <HamburgerDrawer
-        // setCategory={setCategory}
-        //setNewsCity={setNewsCity}
         setCatAndCity={setCatAndCity}
         catAndCity={catAndCity}
         allCategory={allCategory}
--- client/src/HamburgerDrawer.js
+++ client/src/HamburgerDrawer.js
@@ -1,27 +1,12 @@
 import React from "react";
-
 import clsx from "clsx";
-
 import { makeStyles } from "@material-ui/core/styles";
-
 import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
-
 import Button from "@material-ui/core/Button";
-
 import List from "@material-ui/core/List";
-
 import Divider from "@material-ui/core/Divider";
-
 import ListItem from "@material-ui/core/ListItem";
-
-// import ListItemIcon from "@material-ui/core/ListItemIcon";
-
 import ListItemText from "@material-ui/core/ListItemText";
-
-// import InboxIcon from "@material-ui/icons/MoveToInbox";
-
-// import MailIcon from "@material-ui/icons/Mail";
-
 import MenuIcon from "@material-ui/icons/Menu"; // added
 
 import {
@@ -30,34 +15,32 @@ import {
   useMediaQuery,
 } from "@material-ui/core"; // added
 
+//To set width and padding of list
 const useStyles = makeStyles({
   list: {
-    width: 200, // changed
-    paddingLeft: 10, // changed
-    paddingRight: 5, //changed
+    width: 200,
+    paddingLeft: 10,
+    paddingRight: 5,
   },
-
   fullList: {
     width: "auto",
   },
 });
-
+// component created
 export default function SwipeableTemporaryDrawer({
   setCatAndCity,
   allCategory,
   allNewsCity,
   catAndCity,
 }) {
-  console.log(allCategory);
   const classes = useStyles();
   const [state, setState] = React.useState({
     left: false,
   });
 
-  //---------------------------------------------------
+  //-------------------Code to add dark theme--------------------------------
 
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-
   const theme = React.useMemo(
     () =>
       createMuiTheme({
@@ -157,4 +140,3 @@ export default function SwipeableTemporaryDrawer({
     </div>
   );
 }
-// completed
