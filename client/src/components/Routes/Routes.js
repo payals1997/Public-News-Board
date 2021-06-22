3261ce280c3278fce095cf75c86310a1b9c284f0
Rahul Raghunath Chaudhari <rahulchau@cybage.com>
2021-06-22 20:15:04 +0530

    Footer created.

--- client/src/components/Routes/Routes.js
+++ client/src/components/Routes/Routes.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "../Navbar";
-
+import Footer from "../Footer";
 
 function Routes() {
   return (
     <BrowserRouter>
       <Navbar />
-      
+      <Footer />
     </BrowserRouter>
   );
 }
