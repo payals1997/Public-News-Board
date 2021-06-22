a55e7031eb6cbe40a374479ae3383025ff990eae
shubhamCybage16 <Mycybage1@>
2021-06-22 16:07:47 +0530

    persistent side bar done, selected item coloring pending

--- client/src/HamburgerDrawer.js
+++ client/src/HamburgerDrawer.js
@@ -1,142 +1,199 @@
 import React from "react";
-import clsx from "clsx";
-import { makeStyles } from "@material-ui/core/styles";
-import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
-import Button from "@material-ui/core/Button";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import Toolbar from "@material-ui/core/Toolbar";
 import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import MenuIcon from "@material-ui/icons/Menu"; // added
+//import HamburgerApp from "./HambergerApp.css";
 
-import {
-  createMuiTheme,
-  ThemeProvider,
-  useMediaQuery,
-} from "@material-ui/core"; // added
+const drawerWidth = 240;
 
-//To set width and padding of list
-const useStyles = makeStyles({
-  list: {
-    width: 200,
-    paddingLeft: 10,
-    paddingRight: 5,
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
   },
-  fullList: {
-    width: "auto",
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
   },
-});
-// component created
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+}));
+
 export default function SwipeableTemporaryDrawer({
   setCatAndCity,
   allCategory,
   allNewsCity,
   catAndCity,
 }) {
+  console.log(catAndCity);
   const classes = useStyles();
-  const [state, setState] = React.useState({
-    left: false,
-  });
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
 
-  //-------------------Code to add dark theme--------------------------------
-
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
-  const theme = React.useMemo(
-    () =>
-      createMuiTheme({
-        palette: {
-          type: prefersDarkMode ? "light" : "dark",
-        },
-      }),
-
-    [prefersDarkMode]
-  );
-
-  //----------------------------------------------------
-
-  const toggleDrawer = (anchor, open) => (event) => {
-    open === true
-      ? setCatAndCity({ category: "all", city: "all" })
-      : console.log("test");
-    if (
-      event &&
-      event.type === "keydown" &&
-      (event.key === "Tab" || event.key === "Shift")
-    ) {
-      return;
-    }
-
-    setState({ ...state, [anchor]: open });
+  const handleDrawerOpen = () => {
+    setOpen(true);
   };
 
-  const list = (anchor) => (
-    <div
-      className={clsx(classes.list, {
-        [classes.fullList]: anchor === "top" || anchor === "bottom",
-      })}
-      role="presentation"
-    >
-      <List>
-        <ListItem>Categories</ListItem>
-      </List>
-
-      <Divider />
-
-      <List>
-        {allCategory.map((text, index) => (
-          <ListItem
-            style={{ height: 40, borderRadius: 3 }}
-            button
-            onClick={() => setCatAndCity({ ...catAndCity, category: text })}
-            key={text}
-          >
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        <ListItem>Cities</ListItem>
-      </List>
-
-      <Divider />
-
-      <List>
-        {allNewsCity.map((text, index) => (
-          <ListItem
-            style={{ height: 40, borderRadius: 3 }}
-            button
-            onClick={() => setCatAndCity({ ...catAndCity, city: text })}
-            key={text}
-          >
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <div>
-      <Button onClick={toggleDrawer("left", true)}>
-        {/* hamberger added icon, we have not created MenuIcon ,we have directly imported it from material UI and used it here */}
-        <MenuIcon />
-      </Button>
-
-      {/* added */}
-
-      {/* ThemeProvider is also taken from material ui, you can directly use it and put the theme what you have copied from the material ui*/}
-
-      <ThemeProvider theme={theme}>
-        <SwipeableDrawer
-          anchor={"left"}
-          open={state["left"]}
-          onClose={toggleDrawer("left", false)}
-          onOpen={toggleDrawer("left", true)}
+    <div className={classes.root}>
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          //    aria-label="open drawer"
+          onClick={handleDrawerOpen}
+          //edge="start"
+          // className={clsx(classes.menuButton, open && classes.hide)}
         >
-          {list("left")}
-        </SwipeableDrawer>
-      </ThemeProvider>
+          <MenuIcon />
+        </IconButton>
+      </Toolbar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          <ListItem
+            button
+            onClick={() => setCatAndCity({ city: "all", category: "all" })}
+          >
+            <ListItemText
+              primary={
+                <Typography style={{ color: "black" }}> All News </Typography>
+              }
+            />
+          </ListItem>
+        </List>
+        <Divider />
+        <List>
+          <ListItem>
+            <ListItemText
+              primary={
+                <Typography style={{ color: "grey" }}> Categories </Typography>
+              }
+            />
+          </ListItem>
+        </List>
+
+        <Divider />
+
+        <List>
+          {allCategory.map((text, index) => (
+            <ListItem
+              // style={{ backgroundColor: "grey" }}
+              button
+              onClick={() => {
+                setCatAndCity({ ...catAndCity, category: text });
+              }}
+              key={text}
+            >
+              <ListItemText
+                primary={
+                  <Typography style={{ color: "black" }}> {text} </Typography>
+                }
+              />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          <ListItem>
+            <ListItemText
+              primary={
+                <Typography style={{ color: "grey" }}> Cities </Typography>
+              }
+            />
+          </ListItem>
+        </List>
+
+        <Divider />
+        <List>
+          {allNewsCity.map((text, index) => (
+            <ListItem
+              button
+              onClick={() => setCatAndCity({ ...catAndCity, city: text })}
+              key={text}
+            >
+              <ListItemText
+                primary={
+                  <Typography style={{ color: "black" }}> {text} </Typography>
+                }
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
     </div>
   );
 }
