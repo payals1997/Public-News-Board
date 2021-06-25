4c03bf90404b824b718507214b6f76082c067154
Sharyu Suresh Ubale <sharyuu@cybage.com>
2021-06-25 12:34:58 +0530

    Social share functionality updated.

--- client/src/components/SocialMedia/SocialMedia.js
+++ client/src/components/SocialMedia/SocialMedia.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import {FacebookShareButton,WhatsappShareButton,FacebookIcon,WhatsappIcon,TwitterShareButton,TwitterIcon} from 'react-share'
 
-export default function Whatsapp({id}) {
+export default function SocialMedia({id}) {
      var blogId={id}
       var url=`http://localhost:3000/${blogId}`
     return (
         <div>
-           <WhatsappShareButton url = {url}>
+           <WhatsappShareButton url = "https://github.com/">
                <WhatsappIcon logoFillColor = "white" round={true} size="40px"></WhatsappIcon>
                </WhatsappShareButton>
                
@@ -14,7 +14,7 @@ export default function Whatsapp({id}) {
                <FacebookIcon logoFillColor = "white" round={true} size="40px"></FacebookIcon>
                </FacebookShareButton> 
 
-               <TwitterShareButton url = {url}>
+               <TwitterShareButton url = "http://localhost:3000/readmore/1">
                <TwitterIcon logoFillColor = "white" round={true} size="40px"></TwitterIcon>
                </TwitterShareButton>
         </div>
