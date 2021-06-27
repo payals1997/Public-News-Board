ebb4b9aae4261dd4ed99a1667bcb2034f91730c1
Shardul Manish Shete <shardulsh@cybage.com>
2021-06-27 11:31:23 +0530

    client/Routes updated and completed

--- client/src/components/Routes/Routes.js
+++ client/src/components/Routes/Routes.js
@@ -1,21 +1,36 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import Register from '../Register/Register';
-import Login from '../Login'
+import ReadMore from "../NewsBlogs/ReadMore"
+import '../../App.css';
+import Dashboard from "../Dashboard/Dashboard";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from "../Homepg/Home"
+import Register from "../Register";
+import Login from "../Login/Login";
 import Logout from "../Logout/Logout";
+import Footer from "../Footer/Footer";
+import Statistics from "../Statistics";
+import AddNews from "../AddNews" 
 
+const Routing = () => {
+  return (
+    <>
+      <Router>
 
-function Routes() {
-    return (
-        <Router> 
-            <Switch>
-                <Route path="/logout" component={Logout} />
-                <Route path="/signin" exact component={Login} />
-                <Route path="/signup" exact component={Register} />
-            </Switch>
-        </Router>
-    );
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/signup" exact component={Register}></Route>
+          <Route path="/signin" exact component={Login}></Route>
+          <Route path="/dashboard" exact component={Dashboard}></Route>
+          <Route path="/readmore/:id" exact component={ReadMore}></Route>
+          <Route path="/logout" exact component={Logout}></Route>
+          <Route path="/addNews" exact component={AddNews}></Route>
+          <Route path="/statistics" exact component={Statistics}></Route>
+        </Switch>
+      </Router>
+      <div>
+        <Footer />
+      </div>
+    </>
+  )
 }
 
-export default Routes;
-
+export default Routing;
